1d5665ea39f438f2025ef7d3059590e417a953fc
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-05 17:01:01 +0545

    deepest  node

--- tree/treelink.py
+++ tree/treelink.py
@@ -106,10 +106,26 @@ def insert_bt(root_node, new_node):
                 root.value.right_child = new_node
                 return "Node inserted"
 
+# new_node = Tree_node("americano")
+# insert_bt(new_bt, new_node)
+# level_order_traversal(new_bt)
 
-new_node = Tree_node("americano")
-insert_bt(new_bt, new_node)
-level_order_traversal(new_bt)
+def  get_deepest_node(root_node):
+    if not root_node:
+        return
+    else:
+        custom_queue = q.Queue()
+        custom_queue.enqueue(root_node)
+        while not(custom_queue.isEmpty()):
+            root = custom_queue.dequeue()
+            if root.value.left_child is not None:
+                custom_queue.enqueue(root.value.left_child)
+            if root.value.right_child is not None:
+                custom_queue.enqueue(root.value.right_child)
+        deepest_node = root.value
+        return deepest_node
+
+#print(get_deepest_node(new_bt).data)
 
 
 
