16585c9aec1c398a313dc0daee25f0cf42984ef2
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-06 11:01:01 +0545

    insert node

--- tree/tree_list.py
+++ tree/tree_list.py
@@ -4,7 +4,17 @@ class Binary_tree:
         self.last_used_index = 0
         self.max_size = size
 
+    def insert_node(self, value):
+        if self.last_used_index + 1 == self.max_size:
+            return "The Binary tree is full"
+        self.custom_list[self.last_used_index + 1] = value
+        self.last_used_index +=1
+        return "The value has been successfully inserted"
+
 
 
 new_bt = Binary_tree(8)
+print(new_bt.insert_node("Drinks"))
+print(new_bt.insert_node("Hot"))
+print(new_bt.insert_node("Cold"))
 
