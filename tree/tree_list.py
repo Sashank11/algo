1f8b9ea90f436354a313f699c5158150c7958f46
Sashank Rijal <rijalsashank.11@gmail.com>
2022-02-06 10:57:21 +0545

    init tree_list

---
+++ tree/tree_list.py
@@ -0,0 +1,10 @@
+class Binary_tree:
+    def __init__(self, size):
+        self.custom_list = size * [None]
+        self.last_used_index = 0
+        self.max_size = size
+
+
+
+new_bt = Binary_tree(8)
+
